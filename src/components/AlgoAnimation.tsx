97d9c4fa794a0d1c7281a7817546f56fe0876ab1
Pluvio <maksjl01@gmail.com>
2024-04-19 12:06:59 +0100

    rendering cards for animation
    Signed-off-by: Pluvio <maksjl01@gmail.com>

--- src/components/AlgoAnimation.tsx
+++ src/components/AlgoAnimation.tsx
@@ -1,12 +1,24 @@
-import { generateColorArray } from "@/util/Colors";
+"use client";
 
-export default function AlgoAnimation() {
-  const cols = generateColorArray("#ffffff", "#000000", 5);
-  console.log(cols);
+import { generateColorArray } from "@/util/Colors";
+import { useState } from "react";
+
+export default function AlgoAnimation({}: Props) {
+  const [data, setData] = useState<number[]>([8, 5, 6, 3, 1, 2, 4, 7]);
+
+  const cols = generateColorArray("#ffffff", "#000000", data.length);
 
   return (
-    <div>
-      <div className="card rounded-md"></div>
+    <div className="flex flex-row w-full h-full items-center justify-center gap-x-4">
+      {data.map((item, index) => (
+        <div
+          key={item}
+          style={{ backgroundColor: cols[index] }}
+          className="w-[60px] h-[100px] rounded-md"
+        />
+      ))}
     </div>
   );
 }
+
+interface Props {}

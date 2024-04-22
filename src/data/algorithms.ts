816492b8e43a4dcd0949ed81b85ce8d966033ec1
Pluvio <maksjl01@gmail.com>
2024-04-22 22:07:51 +0100

    updated algorithms.json and merge_sort
    Signed-off-by: Pluvio <maksjl01@gmail.com>

---
+++ src/data/algorithms.ts
@@ -0,0 +1,12 @@
+import _Algorithms from "./algorithms.json";
+
+const ALGORITHMS = _Algorithms;
+export { ALGORITHMS };
+
+const ALGORITHM_KEYS = ALGORITHMS.map((e) => e.key);
+export type AlgorithmKeys = (typeof ALGORITHM_KEYS)[number];
+
+export const ALGORITHM_KEYS_MAP = ALGORITHM_KEYS.reduce(
+  (a, v) => ({ ...a, [v]: v }),
+  {}
+) as { [Key in AlgorithmKeys]: AlgorithmKeys };

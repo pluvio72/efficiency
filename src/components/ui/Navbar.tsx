4256a66ac84f4934ea18485d91f40733f0063490
Pluvio <maksjl01@gmail.com>
2024-04-25 00:10:00 +0100

    updated navbar
    Signed-off-by: Pluvio <maksjl01@gmail.com>

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,6 +1,28 @@
 "use client";
 
 import Link from "next/link";
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "./menubar";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "./navigation-menu";
 
 export default function Navbar() {
   return (
@@ -8,7 +30,33 @@ export default function Navbar() {
       <div className="flex-1">
         <a className="btn btn-ghost text-xl font-extrabold">efficiency</a>
       </div>
-      <div className="flex-none gap-x-8 flex">
+      <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>Categories</MenubarTrigger>
+          <MenubarContent>
+            <MenubarSub>
+              <MenubarSubTrigger>Algorithms</MenubarSubTrigger>
+              <MenubarSubContent>
+                <MenubarItem>
+                  <Link href="/algorithms/browse">Browse</Link>
+                </MenubarItem>
+                <MenubarItem>
+                  <Link href="/algorithms/compare">Compare</Link>
+                </MenubarItem>
+              </MenubarSubContent>
+            </MenubarSub>
+            <MenubarSeparator />
+            <MenubarItem>Data Structures</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger>Articles</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>Index</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+      {/* <div className="flex-none gap-x-8 flex">
         <Link
           href="/browse"
           className="hover:bg-neutral-800 py-2 px-4 rounded-md font-light"
@@ -21,7 +69,7 @@ export default function Navbar() {
         >
           Articles
         </Link>
-      </div>
+      </div> */}
     </div>
   );
 }

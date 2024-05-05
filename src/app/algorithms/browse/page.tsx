3de21c79b07fbf2ba2943ba88a7abd0cc501fff1
Pluvio <maksjl01@gmail.com>
2024-05-05 23:43:58 +0100

    added filter to browse algos
    Signed-off-by: Pluvio <maksjl01@gmail.com>

--- src/app/algorithms/browse/page.tsx
+++ src/app/algorithms/browse/page.tsx
@@ -2,7 +2,7 @@
 
 import AlgoDetailsModal from "@/components/pages/algorithms/browse/AlgoDetailsModal";
 import { Algorithm } from "@/types";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { Input } from "@/components/ui/input";
 import AlgorithmCard from "@/components/pages/algorithms/AlgorithmCard";
 import { ALGORITHMS } from "@/data/algorithms";
@@ -10,19 +10,30 @@ import { ALGORITHMS } from "@/data/algorithms";
 export default function Browse() {
   const [selected, setSelected] = useState<Algorithm>();
   const [detailModelOpen, setDetailModalOpen] = useState(false);
+  const [filter, setFilter] = useState("");
 
   const onSelect = (algo: Algorithm) => {
     setSelected(algo);
     setDetailModalOpen(true);
   };
 
+  const updateFilter = (e: ChangeEvent<HTMLInputElement>) =>
+    setFilter(e.target.value);
+
   return (
     <div className="min-h-[95vh] p-4 flex flex-col">
       <div>
-        <Input type="text" placeholder="Search" className="grow" />
+        <Input
+          type="text"
+          placeholder="Search"
+          className="grow"
+          onChange={updateFilter}
+        />
       </div>
       <div className="grid grid-cols-2 gap-x-4 py-4">
-        {ALGORITHMS.map((algo) => (
+        {ALGORITHMS.filter((e) =>
+          e.name.toLowerCase().includes(filter.toLowerCase())
+        ).map((algo) => (
           <AlgorithmCard
             onClick={() => onSelect(algo)}
             details={algo}
